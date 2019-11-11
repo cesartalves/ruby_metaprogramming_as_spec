f8a1ec687ceba11ea779ac78579214389357b2ef
cesartalves <cesartalvez@gmail.com>
2019-11-11 14:38:10 -0300

    more array functions

--- spec/primitives/array_spec.rb
+++ spec/primitives/array_spec.rb
@@ -11,5 +11,26 @@ describe Array do
         it "filter" do
             expect([1,2,3].reject { |x| x % 2 == 0}).to eq [1,3]
         end
+
+        it "shift" do
+            expect([1,2,3].shift).to eq 1
+        end
+
+        it "pop" do
+            expect([1,2,3].pop).to eq 3
+        end
+
+        it "drop" do
+            expect([1,2,3].drop 1).to eq [2,3]
+        end
+
+        it "compact" do
+            expect([1,2, nil,3, nil].compact).to eq [1,2,3]
+        end
+
+        it "take_while" do
+            max = 3
+            expect([1,2,3,4].take_while { |n| n <= max}).to eq [1,2,3]
+        end
     end
 end
